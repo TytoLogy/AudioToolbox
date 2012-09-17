d231be944654e354db9eb8c22616c75777189ca0
Sharad Shanbhag <sshanbhag@neomed.edu>
2012-09-17 15:29:09 -0400

    updated buildfft

--- FFT/buildfft.m
+++ FFT/buildfft.m
@@ -5,22 +5,33 @@ function fftfull = buildfft(fftred)
 % AudioToolbox:FFT
 %-------------------------------------------------------------------------
 %
-%	Given the N+1 points of fftred, buildfft() constructs the length 2N
-%	array fftfull
+%	Given the N points of fftred, buildfft() constructs the 
+%  length 2*(N-1) array fftfull
 %
 %	if y = fft(x):
 %		y(1) = constant
 %		y(2) = f1
 %		y(3) = f2
-%		y(1 + N/2) = fmax
-%		y(N) = y*(2)
-%		y(N-1) = y*(3)
+%		y(N) = fmax
+%		y(N+1) = y*(N-1)
+%		y(N+2) = y*(N-2)
+%		y(2*N) = y*(1)
+%
+%		where * indicates complex conjugate
 %
 %	this function is used by the various synthesis routines 
 %-------------------------------------------------------------------------
 % Input Arguments:
 % 	fftred		complex form of the "single-sided spectrum"
-%
+% 					should have form:
+% 							fftred(1) = freq(0) (constant term)
+% 							fftred(2) = freq(1)
+% 							fftred(3) = freq(2)
+% 							.
+% 							.
+% 							.
+% 							fftred(N) = freq(N-1) (max freq term)
+
 %-------------------------------------------------------------------------
 % Output Arguments:
 % 	fftfull		complex, 2-sided (MATLAB) format spectrum, useful for ifft
@@ -45,6 +56,24 @@ function fftfull = buildfft(fftred)
 % 		- fixed issue with length of final vector
 %	17 Sep 2012 (SJS): fixed bug in building fftfull array
 %---------------------------------------------------------------------
+
+
+
+% N is total number of points in the reduced spectrum
+N = length(fftred);
+% NFFT is final fft vector length
+NFFT = 2*(N-1);
+% allocate fftfull output vector
+fftfull = zeros(1, NFFT);
+% first part of fftfull is fftred
+fftfull(1:N) = fftred;
+% second section is computed as:
+%	(1) take fftred(2:(end-1)), since final point (fftred(end)) 
+% 		 is common to both sections
+% 	(2) flip the fftred section around using fliplr (reverse order)
+% 	(3) take complex conjugate of flipped fftred
+fftfull((N+1):end) = conj(fliplr(fftred(2:(end-1))));
+
 
 %{
 
@@ -75,7 +104,9 @@ fftfull(indx1) = fftred;
 fftfull(indx2) = conj(fliplr(fftred(1:(end-1))));
 %}
 
+%---------------------------------------------------------------------
 %**** original algorithm
+%---------------------------------------------------------------------
 %{
 % N is total number of points in the spectrum minus DC component 
 N = length(fftred) - 1;
@@ -87,13 +118,4 @@ fftfull(1:(N+1)) = fftred;
 % (leaving out DC component which is at Sreduced(1))
 fftfull((N+2):(2*N)) = conj(fftred(N:-1:2));
 %}
-
-keyboard
-
-N = length(fftred);
-NFFT = 2*(N-1);
-fftfull = zeros(1, NFFT);
-fftfull(1:N) = fftred;
-fftfull((N+1):end) = conj(fliplr(fftred(2:(end-1))));
-
 
